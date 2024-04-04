a9872863520ee8a15466780bcd952a249762ed48
gopal-devops1 <gopaldevops97@gmail.com>
2024-04-05 02:04:41 +0530

    daws-76s

---
+++ locals/variables.tf
@@ -0,0 +1,16 @@
+variable "isProd" {
+  type = bool
+  default = true
+}
+
+variable "ami_id" {
+  default = "ami-0f3c7d07486cad139"
+}
+
+variable "zone_id" {
+  default = "Z092964422UUECKUM09QG"
+}
+
+variable "domain_name" {
+  default = "devops1.website"
+}
